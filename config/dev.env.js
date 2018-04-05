1d77a2ca0eca6cb4f167a17013b94bc701168daf
Mnickii <caroline.nkirote@andela.com>
2018-04-05 16:35:12 +0300

    Fetch NewsApi

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  API_KEY: '"78b706393213443cbbd071457b421647"',
 })
